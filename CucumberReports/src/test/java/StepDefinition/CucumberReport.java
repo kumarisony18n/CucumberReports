0da57e64797fd106bc311aa2deaad5c5073581b7
abhij <abhij@LAPTOP-J6JNDT39>
2022-04-22 04:40:01 -0400

    First commit

---
+++ CucumberReports/src/test/java/StepDefinition/CucumberReport.java
@@ -0,0 +1,23 @@
+package StepDefinition;
+
+import io.cucumber.java.en.*;
+
+public class CucumberReport {
+	
+	@Given("User is on Welcome Page")
+	public void user_is_on_welcome_page() {
+		 System.out.println("User on welcome page");
+	}
+
+	@When("User clicks on Login button")
+	public void user_clicks_on_login_button() {
+		 System.out.println("User clicks on Login button");
+	}
+
+	@Then("Welcome page should be displayed")
+	public void welcome_page_should_be_displayed() {
+	    System.out.println("Welcome page should be displayed");
+	}
+
+
+}
---
+++ CucumberReports/src/test/resources/Feature/CucumberReport.feature
@@ -0,0 +1,6 @@
+Feature: Check for Login functionality
+
+  Scenario: Welcome Page Login verification
+    Given User is on Welcome Page
+    When User clicks on Login button
+    Then Welcome page should be displayed
